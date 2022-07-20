8aabb481ec965880c00f5dec1b98b6c30e89a422
AlessHub <aless312@gmail.com>
2022-07-20 19:31:12 +0200

    added footer structure & styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,18 @@
 export function Footer(){
     return (
-        <section>
-            
-
-        </section>
+            <footer>
+                <div className="footer_social_media">
+                    <i className="fa-brands fa-instagram"></i>
+                    <i className="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-facebook"></i>
+                </div>
+                <div>
+                    <p className="footer_text">®Todos los derechos Reservados por MotoHackGp.</p>
+                </div>
+                <div className="footer_contact">
+                    <div className="footer_contact_us"><i class="fa-solid fa-envelope"></i>Contact us</div>
+                    <div>example@gmail</div>
+                </div>
+            </footer>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import { List } from "../components/list/List";
+import { Footer } from "../components/footer/Footer";
 
 export function Home(){
 
     return(
-
+        <>
         <List/>
+        <Footer/>
+        </>
 
     )
 }
