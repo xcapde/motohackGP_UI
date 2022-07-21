8fffc8252bf7a6e1a57561cf854526041df70ab4
xcapde <xcapde@hotmail.com>
2022-07-21 03:23:27 +0200

    axios installed and getAll & getById requests implemented in list and details view

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,13 +4,13 @@ export function Footer(){
                 <div className="footer_social_media">
                     <i className="fa-brands fa-instagram"></i>
                     <i className="fa-brands fa-twitter"></i>
-                    <i class="fa-brands fa-facebook"></i>
+                    <i className="fa-brands fa-facebook"></i>
                 </div>
                 <div>
                     <p className="footer_text">®Todos los derechos Reservados por MotoHackGp.</p>
                 </div>
                 <div className="footer_contact">
-                    <div className="footer_contact_us"><i class="fa-solid fa-envelope"></i>Contact us</div>
+                    <div className="footer_contact_us"><i className="fa-solid fa-envelope"></i>Contact us</div>
                     <div>example@gmail</div>
                 </div>
             </footer>
---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,9 @@
+export function Loader(){
+    return (
+        <section>
+
+            <img src="https://www.e-elastika.gr/images/folder/services/tire.png" alt="loader" className="spinner"/>   
+
+        </section>
+    )
+}
