e164aec3f28903d812de4c681d6b3806713fd1b3
xcapde <xcapde@hotmail.com>
2022-07-21 15:44:30 +0200

    navbar in router and navbar 10vh height

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Navbar} from './components/navbar/Navbar';
 function App() {
   return (
     <div className="App">
-        <Navbar/>
+        {/* <Navbar/> */}
 
         <Home/>   
         
