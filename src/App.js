71f95ae0cad7f4d8ef255a91f42bad853cb915a7
Francisco <Gimli22@gmail.com>
2022-07-21 09:43:13 +0200

    error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './style/style.css'
 import { Home } from './pages/Home';
+import { Detail} from '.pages/detail/Detail';
+
 
 function App() {
   return (
     <div className="App">
-
+          <Detail/>
         <Home/>
+        
     </div>
   );
 }
