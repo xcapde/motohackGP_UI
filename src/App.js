482b8ed7100375a57c55678af1e703ac1949abc5
Francisco <Gimli22@gmail.com>
2022-07-22 08:27:55 +0200

    warrning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import './style/style.css'
 import { Home } from './pages/Home';
-import {Navbar} from './components/navbar/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-        {/* <Navbar/> */}
 
         <Home/>   
         
